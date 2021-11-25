f5d83f6585230cded03b8ff851df8c6748a2efe8
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-11-26 02:51:06 +0900

    react様々なcssの使い方

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { CssModules } from "./components/CssModules";
+import { Emotion } from "./components/Emotion";
+import { InlineStyle } from "./components/inlineStyle";
+import { StyledComponents } from "./components/StyledComponents";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
